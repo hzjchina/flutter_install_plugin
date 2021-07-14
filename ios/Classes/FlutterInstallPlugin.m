186b7fd5c836a87f3e0c017f17ceaa1bcd20f137
Jet(子敬) <test@test.com>
2021-07-14 14:19:32 +0800

    update ios

--- ios/Classes/FlutterInstallPlugin.m
+++ ios/Classes/FlutterInstallPlugin.m
@@ -13,28 +13,10 @@
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   }else if([@"gotoAppStore" isEqualToString:call.method]){
-   print(call.arguments ?? "null")
-          guard let urlString = (call.arguments as? Dictionary<String, Any>)?["urlString"] as? String else {
-              result(FlutterError(code: "参数url不能为空", message: nil, details: nil))
-              return
-          }
-          gotoAppStore(urlString: urlString)
+	NSLog(@"FlutterInstallPlugin urlString: ");
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
-
-
- //跳转到应用的AppStore页页面
-    (void)gotoAppStore(urlString: String) {
-        if let url = URL(string: urlString) {
-            //根据iOS系统版本，分别处理
-            if #available(iOS 10, *) {
-                UIApplication.shared.open(url, options: [:],completionHandler: {(success) in })
-            } else {
-                UIApplication.shared.openURL(url)
-            }
-        }
-    }
 
 @end
