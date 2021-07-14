aa775ae53c0af805318ea8ddc6b287f1c90ee135
Jet(子敬) <test@test.com>
2021-07-14 15:17:38 +0800

    update ios2

--- ios/Classes/FlutterInstallPlugin.m
+++ ios/Classes/FlutterInstallPlugin.m
@@ -13,9 +13,40 @@
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   }else if([@"gotoAppStore" isEqualToString:call.method]){
-	NSLog(@"FlutterInstallPlugin urlString: ");
+  
+	  @try {
+		NSString *urlString = [call arguments];
+		
+		if(urlString == nil) {
+			result([FlutterError errorWithCode:@"urlString" message:@"urlString is null" details:NULL]);
+		
+		}else{
+			NSLog(@"FlutterInstallPlugin urlString: %@",urlString);
+			//NSURL *url= [NSURL URLWithString:urlString];
+			//[[UIApplication sharedApplication]openURL:[NSURLURLWithString:url]];
+			[self openScheme:urlString];
+		}
+	   } 
+	   @catch(FlutterError *e) {
+		  result(e);
+	   }
   } else {
-    result(FlutterMethodNotImplemented);
+	result(FlutterMethodNotImplemented);
+  }
+}
+
+- (void)openScheme:(NSString *)scheme {
+  UIApplication *application = [UIApplication sharedApplication];
+  NSURL *URL = [NSURL URLWithString:scheme];
+ 
+  if ([application respondsToSelector:@selector(openURL:options:completionHandler:)]) {
+    [application openURL:URL options:@{}
+       completionHandler:^(BOOL success) {
+      NSLog(@"Open %@: %d",scheme,success);
+    }];
+  } else {
+    BOOL success = [application openURL:URL];
+    NSLog(@"Open %@: %d",scheme,success);
   }
 }
 
